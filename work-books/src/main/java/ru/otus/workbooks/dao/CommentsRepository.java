886f0c8470595b127bfc8f7bf19cf9d19e28c85c
Овчаренко Андрей Александрович <AAleksOvcharenko@sberbank.ru>
2022-04-15 11:22:40 +0300

    fix избыточного метода

--- work-books/src/main/java/ru/otus/workbooks/dao/CommentsRepository.java
+++ work-books/src/main/java/ru/otus/workbooks/dao/CommentsRepository.java
@@ -4,12 +4,9 @@ import org.springframework.data.repository.CrudRepository;
 import ru.otus.workbooks.entity.Comment;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface CommentsRepository extends CrudRepository<Comment, Long> {
     List<Comment> findByBookId(long id);
-
-    Optional<Comment> findById(Long id);
 
     void removeById(long id);
 }
