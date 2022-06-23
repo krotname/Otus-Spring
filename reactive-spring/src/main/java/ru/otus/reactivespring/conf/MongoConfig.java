de06518615115b328ebd41a2e91532b39df279f7
Овчаренко Андрей Александрович <AAleksOvcharenko@sberbank.ru>
2022-06-23 16:33:30 +0300

    webflux v1

---
+++ reactive-spring/src/main/java/ru/otus/reactivespring/conf/MongoConfig.java
@@ -0,0 +1,14 @@
+package ru.otus.reactivespring.conf;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.core.MongoTemplate;
+
+import java.io.IOException;
+
+@Configuration
+public class MongoConfig {
+
+
+
+}
