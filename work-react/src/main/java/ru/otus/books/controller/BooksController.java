90b86d28bf02b6d71047b4a16cfc563a4034f59d
Овчаренко Андрей Александрович <AAleksOvcharenko@sberbank.ru>
2022-07-20 21:39:04 +0300

    Hystrix add

--- work-react/src/main/java/ru/otus/books/controller/BooksController.java
+++ work-react/src/main/java/ru/otus/books/controller/BooksController.java
@@ -1,6 +1,8 @@
 package ru.otus.books.controller;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import ru.otus.books.dao.BookRepository;
@@ -18,17 +20,22 @@ public class BooksController {
     private final BookRepository repository;
 
     @GetMapping("/api/books/all")
+    @HystrixCommand(fallbackMethod = "defaultListDto")
+    @SneakyThrows
     public List<BookDto> getAllBooks() {
+//        Thread.sleep(50000);
         return repository.findAll().stream().map(BookDto::toDto).collect(Collectors.toList());
     }
 
     @GetMapping("/api/books/{id}")
+    @HystrixCommand(fallbackMethod = "defaultDto")
     public BookDto getBookByIdInPath(@PathVariable("id") long id) {
         Book book = repository.findById(id).orElseThrow(NotFoundException::new);
         return BookDto.toDto(book);
     }
 
     @PostMapping("/api/books")
+    @HystrixCommand
     public BookDto createNewBook(@RequestBody BookDto dto) {
         Book book = BookDto.toDomainObject(dto);
         Book savedBook = repository.save(book);
@@ -36,6 +43,7 @@ public class BooksController {
     }
 
     @PatchMapping("/api/books/{id}/{name}")
+    @HystrixCommand
     public BookDto updateNameById(@PathVariable("id") long id, @RequestParam("name") String name) {
         Book book = repository.findById(id).orElseThrow(NotFoundException::new);
         book.setName(name);
@@ -43,9 +51,22 @@ public class BooksController {
     }
 
     @DeleteMapping("/api/books/{id}")
+    @HystrixCommand(fallbackMethod = "defaultString")
     public String deleteById(@PathVariable("id") long id) {
         repository.deleteById(id);
         return "Ok";
+    }
+
+    private String defaultString(long id) {
+        return "error on load";
+    }
+
+    private BookDto defaultDto(long id) {
+        return new BookDto(0, "");
+    }
+
+    private List<BookDto> defaultListDto() {
+        return List.of(new BookDto(0, ""));
     }
 
     @ExceptionHandler(NotFoundException.class)
